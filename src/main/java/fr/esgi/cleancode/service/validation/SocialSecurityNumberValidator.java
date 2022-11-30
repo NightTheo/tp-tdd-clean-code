ab0b92d393239fa05bc38d0e71c64572bbcc1825
Nigth <omnes.theo@gmail.com>
2022-11-30 11:17:09 +0100

    green SocialSecurityNumberValidator

--- src/main/java/fr/esgi/cleancode/service/validation/SocialSecurityNumberValidator.java
+++ src/main/java/fr/esgi/cleancode/service/validation/SocialSecurityNumberValidator.java
@@ -1,9 +1,24 @@
 package fr.esgi.cleancode.service.validation;
 
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
 import fr.esgi.cleancode.model.DrivingLicence;
+
+import java.util.Objects;
 
 public class SocialSecurityNumberValidator {
 
     public void validate(DrivingLicence drivingLicence) {
+        final var socialSecurityNumber = drivingLicence.getDriverSocialSecurityNumber();
+        if(Objects.isNull(socialSecurityNumber)){
+            throw new InvalidDriverSocialSecurityNumberException("Driver social security number should not be null");
+        }
+
+        if(!socialSecurityNumber.matches("\\d+")) {
+            throw new InvalidDriverSocialSecurityNumberException("Driver social security number should contain only digits");
+        }
+
+        if(socialSecurityNumber.length() != 15) {
+            throw new InvalidDriverSocialSecurityNumberException("Driver social security number should contain 15 digits");
+        }
     }
 }
