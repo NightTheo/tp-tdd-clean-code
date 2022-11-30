8db288af26094a1f76992265fb2272e4ca380502
Pbonnamy <bonnamy.77@gmail.com>
2022-11-30 10:38:59 +0100

    red test find driving licence by id (should find)

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -1,11 +1,18 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
@@ -18,7 +25,18 @@ class DrivingLicenceFinderServiceTest {
 
     @Test
     void should_find() {
+        UUID id = UUID.randomUUID();
+        final var drivingLicence = DrivingLicence
+                .builder()
+                .id(id)
+                .build();
+        when(database.findById(id)).thenReturn(Optional.of(drivingLicence));
 
+        final var found = service.findById(id);
+        assertThat(found).isPresent();
+        assertThat(found).containsSame(drivingLicence);
+        verify(database).findById(id);
+        verifyNoMoreInteractions(database);
     }
 
     @Test
